b2b2f002c64d4f353e458e1cf35a3336967bb02c
Yurchuk Oleg <xauz151@gmail.com>
2020-01-03 01:56:48 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // root
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: MyHomePage(title: 'First flutter application'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final Random _random = Random();
+
+  Color _color = Color(0xFFFFFFFF);
+
+  void _changeColor() {
+    setState(() {
+      _color = Color.fromARGB(
+        _random.nextInt(256),
+        _random.nextInt(256),
+        _random.nextInt(256),
+        _random.nextInt(256),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.title,
+          style: TextStyle(
+            fontSize: 24.0
+        )
+      ),
+    ),
+      body: Material(
+          child: InkWell(
+            onTap: _changeColor,
+            child: Container(color: _color,
+              child: Center(
+                child: Text(
+                  'Hey there',
+                    style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue
+                  ))
+              )))
+            )
+          );}}
